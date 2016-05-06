42ed0de8c763986068f1a5b282b5454265664f6e
ochowie <ochowie>
2016-05-05 17:33:08 -0700

    Converting reducer first. Will need to switch to Object.assign

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/react-dom/index.d.ts" />
 /// <reference path="browser/ambient/react-redux/index.d.ts" />
 /// <reference path="browser/ambient/react/index.d.ts" />
